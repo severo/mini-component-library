19b6f036a1d8e2b396092b93f194c39da007e2c1
Sylvain Lesage <severo@rednegra.net>
2025-03-21 23:32:24 +0100

    Select
    missing:
    - cannot have the width adapted to the content on Firefox
    - the chevron image does not react to click. Should I add an onClick
      event listener?

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <OuterWrapper>
+      <StyledIcon id="chevron-down" size="24" strokeWidth="2" />
+      <Wrapper value={value} onChange={onChange}>
+        {children}
+      </Wrapper>
+    </OuterWrapper>
   );
 };
+
+const OuterWrapper = styled.div`
+  position: relative;
+  width: fit-content;
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Wrapper = styled.select`
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  font-size: 1rem;
+  font-weight: 400;
+  font-family: "Roboto", sans-serif;
+  line-height: 1;
+  padding: 0.75rem 3.25rem 0.75rem 1rem;
+  /* position: relative; */
+  /* remove the native chevron */
+  appearance: none;
+  /* set the width to the content (does not work with firefox) */
+  field-sizing: content;
+
+  &:focus {
+    outline-color: ${COLORS.primary};
+  }
+`;
+
+const StyledIcon = styled(Icon)`
+  position: absolute;
+  right: 12px;
+  top: calc(50% - 12px);
+`;
 
 export default Select;
