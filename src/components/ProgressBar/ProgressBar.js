91ce5169db13e09980c6b4e46c11cf268450f474
Sylvain Lesage <severo@rednegra.net>
2025-03-21 22:19:40 +0100

    progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,61 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  large: {
+    "--padding": "4px",
+    "--height": "16px",
+    "--radius": "4px",
+  },
+  medium: {
+    "--padding": "0",
+    "--height": "12px",
+    "--radius": "4px",
+  },
+  small: {
+    "--padding": "0",
+    "--height": "8px",
+    "--radius": "4px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={SIZES[size] ?? SIZES.medium}
+    >
+      <Bar role="presentation" style={{ "--width": value + "%" }} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.span`
+  display: inline-block;
+  width: 100%;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: calc(var(--radius) + var(--padding));
+  padding: var(--padding);
+`;
+
+const Bar = styled.span`
+  display: block;
+  width: 100%;
+  background: linear-gradient(
+    to right,
+    ${COLORS.primary} 0% var(--width),
+    transparent var(--width)
+  );
+  height: var(--height);
+  border-radius: var(--radius);
+`;
 
 export default ProgressBar;
